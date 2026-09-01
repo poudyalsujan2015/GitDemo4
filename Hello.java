7dc4487fb8978cc5fce2fe5a697dd6d784ca0bc9
Sujan <poudyal.sujan2015@gmail.com>
2026-09-01 10:44:12 -0500

    updated the welcome message

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello to git!");
+        System.out.println("Hello CS 3354!");
         System.out.println("CS3354 Object-Oriented Design & Programming");
     }
 }
